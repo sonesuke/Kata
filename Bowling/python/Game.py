cb2accd471132a8d17a5ba09e47abaa390b8fb2a
sonesuke <iamsonesuke@gmail.com>
2012-08-04 18:14:41 +0900

    20120804 Kata

--- Bowling/python/Game.py
+++ Bowling/python/Game.py
@@ -1,54 +1,80 @@
+class Frame:
+
+    def __init__(self):
+        self.rolls = []
+        self.next_frame = None
+
+    def is_full(self):
+        if self.is_strike():
+            return True
+        return len(self.rolls) >= 2
+
+    def add(self, pins):
+        if sum(self.rolls + [pins]) > 10:
+            raise ValueError
+        self.rolls += [pins]
+
+    def is_spare(self):
+        if len(self.rolls) != 2:
+            return False
+        return sum(self.rolls[:2]) == 10
+
+    def is_strike(self):
+        return len(self.rolls) == 1 and self.rolls[0] == 10
+
+    @property
+    def score(self):
+        ret = sum(self.rolls)
+        if self.next_frame:
+            if self.is_spare():
+                ret += sum(self.next_frame.rolls[:1])
+            if self.is_strike():
+                ret += sum(self.next_frame.rolls[:2])
+        return ret
+
+
+class Frame10(Frame):
+    def is_full(self):
+        if sum(self.rolls[:2]) >= 10:
+            return 3 <= len(self.rolls)
+        else:
+            return 2 <= len(self.rolls)
+
+    def add(self, pins):
+        if pins > 10:
+            raise ValueError
+        if len(self.rolls) == 1:
+            if self.rolls[0] < 10 and self.rolls[0] + pins > 10:
+                raise ValueError
+        self.rolls += [pins]
+
+    @property
+    def score(self):
+        ret = sum(self.rolls)
+        if self.rolls[0]:
+            if self.rolls[0] != 10 and sum(self.rolls[0:2]) == 10:
+                ret += sum(self.rolls[2:3])
+            if self.rolls[0] == 10:
+                ret += sum(self.rolls[1:3])
+        return ret
 
 
 class Game:
+
     def __init__(self):
-        self.scores = []
-        self.frames = []
+        self.frames = [Frame()]
 
     def roll(self, pins):
-        if 10 < pins:
-            raise ValueError
-
-        if len(self.frames) == 0:
-            self.frames += [[pins]]
-            return
-
-        if len(self.frames) < 10:
-            if len(self.frames[-1]) < 2 and sum(self.frames[-1]) != 10:
-                if 10 < sum(self.frames[-1]) + pins:
-                    raise ValueError
-                self.frames[-1] += [pins]
-            else:
-                self.frames += [[pins]]
-        elif len(self.frames) == 10:
-            if self.frames[-1][0] == 10 and len(self.frames[-1]) < 2:
-                self.frames[-1] += [pins]
-            elif 2 == len(self.frames[-1]):
-                if sum(self.frames[-1]) >= 10:
-                    self.frames[-1] += [pins]
-                else:
-                    raise ValueError
+        if self.frames[-1].is_full():
+            if len(self.frames) < 9:
+                self.frames += [Frame()]
+            elif len(self.frames) == 9:
+                self.frames += [Frame10()]
             else:
                 raise ValueError
+            self.frames[-2].next_frame = self.frames[-1]
+        self.frames[-1].add(pins)
 
-        else:
-            raise ValueError
-
+    @property
     def score(self):
-        ret = 0
-        for idx in range(0, len(self.frames)):
-            if 0 < idx:
-                if self.frames[idx - 1][0] == 10:
-                    ret += sum(self.frames[idx])
-                elif sum(self.frames[idx - 1]) == 10:
-                    ret += self.frames[idx][0]
-            ret += sum(self.frames[idx])
-            if 9 == idx:
-                if self.frames[idx][0] == 10:
-                    ret += sum(self.frames[idx][1:])
-                elif sum(self.frames[idx][:1]) == 10:
-                    ret += self.frames[idx][2]
-        return ret
-
-    def frame(self):
-        return self.frames
+        return sum(map(lambda x: x.score, self.frames))
--- Bowling/python/test_Game.py
+++ Bowling/python/test_Game.py
@@ -2,81 +2,69 @@ import pytest
 from Game import Game
 
 
-def test_craete():
+def test_create():
     g = Game()
-    assert g
 
 
 def test_roll():
     g = Game()
     g.roll(3)
-
-
-def test_spare():
-    g = Game()
-    g.roll(0)
-    g.roll(1)
-    g.roll(2)
-    g.roll(8)
-    g.roll(4)
-    g.roll(5)
-    assert g.frame() == [[0, 1], [2, 8], [4, 5]]
-    assert g.score() == 1 + 10 + 4 + 9
-
-
-@pytest.mark.parametrize(("input", "frame_expected", "score_expetected"), [
-    ([1, 2, 3, 4, 5], [[1, 2], [3, 4], [5]], 1 + 2 + 3 + 4 + 5), # normal case
-    ([10, 1, 2, 3, 4, 5], [[10], [1, 2], [3, 4], [5]], 10 + 3 + 3 + 7 + 5), # one strike
-    ([10, 1, 2, 10, 10, 5], [[10], [1, 2], [10], [10], [5]], 10 + 3 + 3 + 10 + 10 + 10 + 5 + 5), # two strike
-    ([0, 1, 2, 8, 4, 5], [[0, 1], [2, 8], [4, 5]], 1 + 10 + 4 + 9),
-])
-def test_frame(input, frame_expected, score_expetected):
-    g = Game()
-    for i in input:
-        g.roll(i)
-    assert g.frame() == frame_expected
-    assert g.score() == score_expetected
-
-
-def test_frames_over():
-    g = Game()
-    for i in range(0, 9):
-        g.roll(1)
-        g.roll(1)
-    g.roll(1)
-    g.roll(1)
     with pytest.raises(ValueError):
-        g.roll(1)
-
-
-def test_score():
-    g = Game()
-    assert g.score() == 0
-    g.roll(3)
-    assert g.score() == 3
-
-
-def test_roll_ten_over():
-    g = Game()
+        g.roll(8)
+    g.roll(4)
     with pytest.raises(ValueError):
         g.roll(11)
 
 
-def test_frame_ten_over():
+def test_score():
+    g = Game()
+    assert g.score == 0
+    g.roll(3)
+    assert g.score == 3
+
+
+def test_spare():
     g = Game()
     g.roll(3)
-    with pytest.raises(ValueError):
-        g.roll(8)
+    g.roll(7)
+    g.roll(5)
+    g.roll(3)
+    assert g.score == 23
 
 
-def test_10_frame():
+def test_strike():
     g = Game()
-    for idx in range(0, 9):
-        g.roll(1)
-        g.roll(1)
     g.roll(10)
-    g.roll(2)
     g.roll(3)
-    print g.frame()
-    assert g.frame()[-1] == [10, 2, 3]
-    assert g.score() == 2 * 9 + 10 + 5 + 5
+    g.roll(5)
+    g.roll(3)
+    assert g.score == 29
+
+
+@pytest.mark.parametrize(
+    ('rolls', 'expected'),
+    [
+        ([1, 2], 18 + 3),
+        ([10, 5, 5], 18 + 30),
+        ([5, 5, 10], 18 + 30),
+    ]
+)
+def test_10_frame(rolls, expected):
+    g = Game()
+    for i in range(0, 18):
+        g.roll(1)
+
+    for r in rolls:
+        g.roll(r)
+    assert g.score == expected
+    with pytest.raises(ValueError):
+        g.roll(1)
+
+
+def test_10_frame_2nd_roll():
+    g = Game()
+    for i in range(0, 18):
+        g.roll(1)
+    g.roll(2)
+    with pytest.raises(ValueError):
+        g.roll(9)
